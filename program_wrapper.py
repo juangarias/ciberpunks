be2c2232602d9fa802090c4469ef6a6c4993f270
juan <juan@finisterre.(none)>
2015-10-26 21:45:48 -0300

    Formateo de codigo con 4 espacios por tab. Cambio en la deteccion del sistema operativo.

--- program_wrapper.py
+++ program_wrapper.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 # coding=utf-8
 
-import os, sys
+import os
+import sys
 
 if len(sys.argv) > 1:
-  status = 9
-  while status == 9:
-    status = int(os.system ("./" + sys.argv[1]) / 256) # exit code is upper 8 bits
+    status = 9
+    while status == 9:
+        status = int(os.system("python " + sys.argv[1]) / 256)  # exit code is upper 8 bits
 
 else:
-  print "Usage: program_wrapper.py [your_program]"
+    print "Usage: program_wrapper.py [your_program]"
--- window_input.py
+++ window_input.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # coding=utf-8
 
-import argparse, logging, os, sys
+import argparse
+import logging
+import os
+import platform
+import sys
 import cv2
 from curses import *
 from common import configureLogging, encodeSubjectPictureName, calculateScaledSize, drawLabel
@@ -9,170 +13,170 @@ from common import configureLogging, encodeSubjectPictureName, calculateScaledSi
 ESCAPE_KEY = 27
 BACKSPACE_KEY = 263
 
-if os.name == 'posix':
-  ENTER_KEY = 13
+if platform.system() == 'Darwin':
+    ENTER_KEY = 13
 else:
-  ENTER_KEY = 10
+    ENTER_KEY = 10
 
 
 def configureArguments():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--outFolder', help="Folder for writing collected faces.", 
-    default="/home/juan/ciberpunks/faces/news")
-  parser.add_argument('--outputWidth', help="Output with for images to display in windows.", default="600")
-  parser.add_argument('--log', help="Log level for logging.", default="WARNING")
-  return parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--outFolder', help="Folder for writing collected faces.",
+                        default="/home/juan/ciberpunks/faces/news")
+    parser.add_argument('--outputWidth', help="Output with for images to display in windows.",
+                        default="600")
+    parser.add_argument('--log', help="Log level for logging.", default="WARNING")
+    return parser.parse_args()
 
 
 def readInput(stdscr, y, x):
-  stdscr.move(y, x)
-  value = ''
-  c = stdscr.getch()
-
-  invalidChars = list('\\ºª!|·$%/()=¡^`[]{}*+:;<>,')
-
-  while c != 10:
-    if c < 256:
-      if chr(c) in invalidChars:
-        stdscr.delch(y, x)
-      else:
-        value += chr(c)
-        x += 1
+    stdscr.move(y, x)
+    value = ''
     c = stdscr.getch()
 
-  return value
+    invalidChars = list('\\ºª!|·$%/()=¡^`[]{}*+:;<>,')
+
+    while c != 10:
+        if c < 256:
+            if chr(c) in invalidChars:
+                stdscr.delch(y, x)
+            else:
+                value += chr(c)
+                x += 1
+        c = stdscr.getch()
+
+    return value
 
 
 def drawInputWindow(stdscr):
-  stdscr.clear()
-  (h, w) = stdscr.getmaxyx()
-  yPos = int(h*0.4)
-  xPos = int (w*0.4)
+    stdscr.clear()
+    (h, w) = stdscr.getmaxyx()
+    yPos = int(h*0.4)
+    xPos = int(w*0.4)
 
-  stdscr.addstr(yPos, xPos, "================================")
-  yPos += 1
-  stdscr.addstr(yPos, xPos, "|| BIENVENIDOS A CyBerSiBeriA ||")
-  yPos += 1
-  stdscr.addstr(yPos, xPos, "================================")
-  yPos += 3
+    stdscr.addstr(yPos, xPos, "================================")
+    yPos += 1
+    stdscr.addstr(yPos, xPos, "|| BIENVENIDOS A CyBerSiBeriA ||")
+    yPos += 1
+    stdscr.addstr(yPos, xPos, "================================")
+    yPos += 3
 
-  stdscr.addstr(yPos, xPos, "Ingrese su nombre: ")
-  (nameY, nameX) = stdscr.getyx()
-  yPos += 1
-  stdscr.addstr(yPos, xPos, "Ingrese su e-mail: ")
-  (emailY, emailX) = stdscr.getyx()
-  yPos += 3
-  stdscr.addstr(yPos, xPos, "===== NO enviamos spam... =====")
-  yPos += 1
+    stdscr.addstr(yPos, xPos, "Ingrese su nombre: ")
+    (nameY, nameX) = stdscr.getyx()
+    yPos += 1
+    stdscr.addstr(yPos, xPos, "Ingrese su e-mail: ")
+    (emailY, emailX) = stdscr.getyx()
+    yPos += 3
+    stdscr.addstr(yPos, xPos, "===== NO enviamos spam... =====")
+    yPos += 1
 
-  echo()
-  name = readInput(stdscr, nameY, nameX)
-  email = readInput(stdscr, emailY, emailX)
+    echo()
+    name = readInput(stdscr, nameY, nameX)
+    email = readInput(stdscr, emailY, emailX)
 
-  return name, email
-  
+    return name, email
+
 
 def getUserPicture(outputWidth):
 
-  camera = cv2.VideoCapture(0)
-  
-  if not camera.isOpened():
-    logging.error("Arrgghhh! The camera is not working!")
-    return None
+    camera = cv2.VideoCapture(0)
 
-  outputSize = calculateScaledSize(outputWidth, capture=camera)
-  logging.debug("Reading camera...")
-  readOk, image = camera.read()
+    if not camera.isOpened():
+        logging.error("Arrgghhh! The camera is not working!")
+        return None
 
-  picWin = "Sonria..."
-  cv2.namedWindow(picWin)
-
-  key = -1
-
-  while key != ENTER_KEY and readOk:
-    image = cv2.resize(image, outputSize)
-    drawLabel("Presione [Enter]...", image, (int(outputWidth/3), 50))
-    cv2.imshow(picWin, image)
-    key = cv2.waitKey(5) % 256
+    outputSize = calculateScaledSize(outputWidth, capture=camera)
+    logging.debug("Reading camera...")
     readOk, image = camera.read()
 
-  cv2.destroyWindow(picWin)
-  cv2.waitKey(1)
-  cv2.waitKey(2)
-  cv2.waitKey(3)
+    picWin = "Sonria..."
+    cv2.namedWindow(picWin)
 
-  return image
+    key = -1
+
+    while key != ENTER_KEY and readOk:
+        image = cv2.resize(image, outputSize)
+        drawLabel("Presione [Enter]...", image, (int(outputWidth/3), 50))
+        cv2.imshow(picWin, image)
+        key = cv2.waitKey(5) % 256
+        readOk, image = camera.read()
+
+    cv2.destroyWindow(picWin)
+    cv2.waitKey(1)
+    cv2.waitKey(2)
+    cv2.waitKey(3)
+
+    return image
 
 
 def drawThanksWindow(stdscr):
-  stdscr.clear()
-  (h, w) = stdscr.getmaxyx()
-  yPos = int(h*0.4)
-  xPos = int (w*0.4)
+    stdscr.clear()
+    (h, w) = stdscr.getmaxyx()
+    yPos = int(h*0.4)
+    xPos = int(w*0.4)
 
-  stdscr.addstr(yPos, xPos, "================================")
-  yPos += 1
-  stdscr.addstr(yPos, xPos, "||       MUCHAS GRACIAS       ||")
-  yPos += 1
-  stdscr.addstr(yPos, xPos, "================================")
-  yPos += 3
-  stdscr.addstr(yPos, xPos, "...Por favor, presione [Enter]...")
+    stdscr.addstr(yPos, xPos, "================================")
+    yPos += 1
+    stdscr.addstr(yPos, xPos, "||       MUCHAS GRACIAS       ||")
+    yPos += 1
+    stdscr.addstr(yPos, xPos, "================================")
+    yPos += 3
+    stdscr.addstr(yPos, xPos, "...Por favor, presione [Enter]...")
 
-  noecho()
+    noecho()
 
-  c = 0 
-  while c != 10:
-    c = stdscr.getch()
+    c = 0
+    while c != ENTER_KEY:
+        c = stdscr.getch()
 
 
 def initCurses():
-  stdscr = initscr()
-  noecho()
-  cbreak()
-  start_color()
-  stdscr.keypad(1)
-  stdscr.border(0)
-  stdscr.box()
+    stdscr = initscr()
+    noecho()
+    cbreak()
+    start_color()
+    stdscr.keypad(1)
+    stdscr.border(0)
+    stdscr.box()
 
-  return stdscr
+    return stdscr
 
 
 def destroyCurses():
-  nocbreak()
-  echo()
-  endwin()  
-  os.system('stty sane')
+    nocbreak()
+    echo()
+    endwin()
+    os.system('stty sane')
 
 
 def main():
-  try:
-    returnCode = 9
-    args = configureArguments()
-    configureLogging(args.log)
+    try:
+        returnCode = 9
+        args = configureArguments()
+        configureLogging(args.log)
 
-    stdscr = initCurses()
+        stdscr = initCurses()
 
-    name = ''
-    email = ''
+        name = ''
+        email = ''
 
-    while not name or not email:
-      (name, email) = drawInputWindow(stdscr)
+        while not name or not email:
+            (name, email) = drawInputWindow(stdscr)
 
-    img = getUserPicture(int(args.outputWidth))
+        img = getUserPicture(int(args.outputWidth))
 
-    formatParams = [int(cv2.IMWRITE_JPEG_QUALITY), 100]
-    cv2.imwrite(args.outFolder + '/' + encodeSubjectPictureName(name, email) + '.jpg', img, formatParams)
+        formatParams = [int(cv2.IMWRITE_JPEG_QUALITY), 100]
+        cv2.imwrite(args.outFolder + '/' + encodeSubjectPictureName(name, email) + '.jpg', img, formatParams)
 
-    drawThanksWindow(stdscr)
+        drawThanksWindow(stdscr)
 
-  except KeyboardInterrupt:
-    returnCode = 0
-  finally:
-    cv2.destroyAllWindows()
-    destroyCurses()
-    sys.exit(returnCode)
+    except KeyboardInterrupt:
+        returnCode = 0
+    finally:
+        cv2.destroyAllWindows()
+        destroyCurses()
+        sys.exit(returnCode)
 
-    
-  
+
 if __name__ == '__main__':
-  main()
+    main()
