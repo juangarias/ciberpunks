ab7a533232be01b1d5c04c6e88373db5d1435964
Juan <juangarias@gmail.com>
2015-11-04 18:00:44 -0300

    Manejo de errores al conectarse al servidor SSH. Pruebas para verificar el error.

--- tests/ssh_test.py
+++ tests/ssh_test.py
@@ -1,11 +1,14 @@
 import paramiko
 
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect('192.168.1.104', username='juan', password='juan')
-sftpClient = client.open_sftp()
-ret = sftpClient.put('/home/juan/instalacion_ciberpunks/window_input.log', '/Users/juan/faces/news/w.log')
+try:
+  client = paramiko.SSHClient()
+  client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  client.connect('192.168.1.197', username='juan', password='juancito')
+  sftpClient = client.open_sftp()
+  ret = sftpClient.put('/home/juan/instalacion_ciberpunks/window_input.log', '/Users/juan/faces/news/w.log')
 
-print ret
+  print ret
+except Exception as e:
+  print type(e).__name__
 
 client.close()
