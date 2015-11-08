9bce2581bcba4c50afa794108b31e8a57916fbc4
Juan <juangarias@gmail.com>
2015-11-08 14:58:33 -0300

    Cambios en la implementacion del multiple ssh client: - No abrimos al conexion al comienzo, sino que lo hacemos cada vez que queremos enviar un archivo. - Abre y cierra todo en cada envio de cada item.

--- multiple_ssh_client.py
+++ multiple_ssh_client.py
@@ -11,10 +11,9 @@ class MultipleSSHClient(object):
         self.sshServersIPs = sshServerIP.split(',')
         self.sshUsers = sshUser.split(',')
         self.sshPasswords = sshPassword.split(',')
-        self.sshClients = []
 
-        for server, user, pwd in zip(self.sshServersIPs, self.sshUsers, self.sshPasswords):
-            self.sshClients.append(self.openSSH(server, user, pwd))
+        if len(self.sshServersIPs) != len(self.sshUsers) or len(self.sshServersIPs) != len(self.sshPasswords):
+            raise ValueError('Incorrect number of ssh hosts, users and passwords.')
 
     def openSSH(self, sshHost, sshUser, sshPassword):
         try:
@@ -31,24 +30,31 @@ class MultipleSSHClient(object):
             raise paramiko.SSHException('Could not connect to server')
 
     def send(self, filename, localpath, remoteFolder):
-        for sshClient in self.sshClients:
-            logging.debug('Trying to open SFTP client...')
+        i = 0
+        logging.debug('Trying to send file tp {0} servers.'.format(len(self.sshServersIPs)))
+        for sshHost, sshUser, sshPassword in zip(self.sshServersIPs, self.sshUsers, self.sshPasswords):
+            sshClient = self.openSSH(sshHost, sshUser, sshPassword)
+            logging.debug('Trying to open SFTP client for server {0}...'.format(i))
             sftpClient = sshClient.open_sftp()
             logging.debug('Connect to SSH and SFTP server OK.')
 
-            remotepath = remoteFolder + '/' + filename
+            remotepath = filename
             logging.debug('Trying to put remote file to {0}'.format(remotepath))
             ret = sftpClient.put(localpath, remotepath)
             logging.debug('File transferred to server in {0} with result {1}.'.format(remotepath, ret))
 
+            logging.debug('Trying to close sFTP and SSH connections...')
             self.tryClose(sftpClient)
+            self.tryClose(sshClient)
+            logging.debug('Close sFTP and SSH connections OK!')
+            i += 1
 
     def close(self):
-        for client in self.sshClients:
-            self.tryClose(client)
+        pass
 
     def tryClose(self, client):
         try:
             client.close()
         except:
             pass
+
